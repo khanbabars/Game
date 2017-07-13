45190d93f934bc1182b57e153094b5df5bc24444
khanbabars <khanbabars@gmail.com>
2017-07-13 21:46:04 +0200

    logging almost done

--- src/org/datasource/GameSession.java
+++ src/org/datasource/GameSession.java
@@ -2,32 +2,25 @@ package org.datasource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
 import java.sql.SQLException;
 
+
 public class GameSession {
-
-	public static void main(String args[]) throws SQLException {
-	
-		
-		
-	}
-
+	 
+public static void main(String args[]) throws SQLException {
+		}
 	public static void getGameSession(int win, int bet, int balance) throws SQLException {
 
 		// get connection from JdbcConnectionPool
 		Connection conn = JdbcConnectionPool.poolFactoryPrimary().getConnection();
-
 		PreparedStatement insertSql = conn
 				.prepareStatement("insert into game_session(win, bet, balance) values(?,?,?)");
 		insertSql.setInt(1, win);
 		insertSql.setInt(2, bet);
 		insertSql.setInt(3, balance);
 		insertSql.executeUpdate();
-
-		conn.close();
-
-		// System.out.println("New row inserted ");
+	conn.close();
+// System.out.println("New row inserted ");
 
 	}
 }
--- src/org/log/Logging.java
+++ src/org/log/Logging.java
@@ -5,17 +5,14 @@ package org.log;
 import org.apache.log4j.Logger;
 import java.io.IOException;
 
-
-	
-	
-
-	public class Logging {
+public class Logging {
 	  private static final Logger logger = Logger.getLogger(Logging.class);
-	   public static void getLogs() throws IOException {
+	   public static void main(String[] args)  throws IOException {
 		   
 		   //BasicConfigurator.configure();
 	     
 	      logger.info("Starting Application Server");
+	      System.out.println("logs are printing");
 	      /*
 	      logger.debug("DEBUG");
 	      logger.error("ERROR");
