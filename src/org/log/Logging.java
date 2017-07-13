8102895aec5d79a2207776a4304107d376cb607c
khanbabars <khanbabars@gmail.com>
2017-07-13 16:50:40 +0200

    jdbc logging

---
+++ src/org/log/Logging.java
@@ -0,0 +1,28 @@
+package org.log;
+
+
+
+import org.apache.log4j.Logger;
+import java.io.IOException;
+
+
+	
+	
+
+	public class Logging {
+	  private static final Logger logger = Logger.getLogger(Logging.class);
+	   public static void getLogs() throws IOException {
+		   
+		   //BasicConfigurator.configure();
+	     
+	      logger.info("Starting Application Server");
+	      /*
+	      logger.debug("DEBUG");
+	      logger.error("ERROR");
+	      logger.warn("WARN");
+	      logger.fatal("FATAL");
+	      */
+	   }
+	}
+
+
